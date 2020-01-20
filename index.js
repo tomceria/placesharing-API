108892db95357d21a206797764779437ec4ee0ae
Lưu Minh Hoàng <ceriagame@gmail.com>
2020-01-21 00:34:46 +0700

    7_89

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const usersRoutes = require('./routes/users-routes')
+const placesRoutes = require('./routes/places-routes')
 
 const app = express()
 
-app.use(usersRoutes)
+app.use('/api/places', placesRoutes)
 
 app.listen(5000)
