74ae36b2496e72ab69957a7333779c7828ee2362
Lưu Minh Hoàng <ceriagame@gmail.com>
2020-01-21 12:15:55 +0700

    7_93

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,40 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-const HttpError = require('../models/http-error')
+const placeController = require('../controllers/place-controllers')
 
-const DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516
-    },
-    address: '20 W 34th St, New York, NY 10001',
-    creator: 'u1'
-  }
-]
-
-router.get('/:pid', (req, res, next) => {
-  const placeId = req.params.pid
-  console.log(`[PLACES: /:pid] GET /${placeId}`)
-  const place = DUMMY_PLACES.find(p => p.id === placeId)
-  if (!place) {
-    return next(new HttpError('Could not find a place for the provided pid.', 404))
-  }
-  res.json({ place })
-})
-
-router.get('/user/:uid', (req, res, next) => {
-  const userId = req.params.uid
-  console.log(`[PLACES: /user/:uid] GET /${userId}`)
-  const place = DUMMY_PLACES.find(p => p.creator === userId)
-  if (!place) {
-    throw new HttpError('Could not find a place for the provided uid.', 404)
-  }
-  res.json({ place })
-})
+router.get('/:pid', placeController.getPlaceById)
+router.get('/user/:uid', placeController.getPlaceByUserId)
 
 module.exports = router
