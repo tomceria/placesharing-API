98f8300a76a11e6b7f881458fc2b386b1e4005cc
Lưu Minh Hoàng <ceriagame@gmail.com>
2020-01-21 13:37:44 +0700

    7_95

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const HttpError = require('./models/http-error')
 const placesRoutes = require('./routes/places-routes')
 
 const app = express()
 app.use(bodyParser.json())
 
+// Route Handlers
 app.use('/api/places', placesRoutes)
 
+// Undefined route Handler
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404)
+  next(error)
+})
+
+// Error Handler
 app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error)
